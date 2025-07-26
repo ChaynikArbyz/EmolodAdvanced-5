ac21d2c3386980bd1a2de7deb051b3b2125fcdcf
ChaynikArbyz <romikbro.133@gmail.com>
2025-07-26 22:56:43 +0300

    Finished

---
+++ ContactBook-V2/ContactBook-V2/Contacts.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NotesAndPasswordGenerator
+{
+    internal class Contacts
+    {
+        private Dictionary<string, string> contacts = new Dictionary<string, string>();
+
+        public void AddContact(string name, string phoneNumber)
+        {
+            if (contacts.ContainsKey(name))
+            {
+                ColorText.WriteColorLine("Контакт з таким ім'ям вже існує!", ConsoleColor.Red);
+            }
+            else
+            {
+                contacts.Add(name, phoneNumber);
+                ColorText.WriteColorLine("Контакт успішно додано!", ConsoleColor.Green);
+            }
+        }
+
+        public void ShowContacts() 
+        {
+            if (contacts.Count < 1)
+            {
+                ColorText.WriteColorLine("Контакти відсутні!", ConsoleColor.Red);
+
+            }
+            else
+            {
+                 ColorText.WriteColorLine("Список контактів:", ConsoleColor.Yellow);
+                     foreach (KeyValuePair<string,string> kv in contacts)
+                     {
+                         Console.WriteLine(kv.Key + " - " + kv.Value);
+                     }
+            }
+        }
+
+        public void DeleteContact(string name)
+        {
+            if (contacts.ContainsKey(name))
+            {
+                Console.WriteLine($"Ви впевнені, що хочете видалити контакт {name}? (y/n)");
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    contacts.Remove(name);
+                    ColorText.WriteColorLine("\nКонтакт успішно видалено!", ConsoleColor.Green);
+                }
+                else
+                {
+                    ColorText.WriteColorLine("\nВидалення скасовано.", ConsoleColor.Yellow);
+                }
+            }
+            else
+            {
+                ColorText.WriteColorLine("Контакт з таким ім'ям не знайдено!", ConsoleColor.Red);
+            }
+        }
+
+        public void ChangeContact(string name, string newName, string newPhoneNumber)
+        {
+            if (contacts.ContainsKey(name))
+            {
+                contacts.Remove(name);
+                contacts.Add(newName, newPhoneNumber);
+                ColorText.WriteColorLine("Контакт успішно змінено!", ConsoleColor.Green);
+            }
+            else
+            {
+                ColorText.WriteColorLine("Контакт з таким ім'ям не знайдено!", ConsoleColor.Red);
+            }
+        }
+
+    }
+}
--- ContactBook-V2/ContactBook-V2/Program.cs
+++ ContactBook-V2/ContactBook-V2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NotesAndPasswordGenerator;
 
 namespace ContactBook_V2
 {
@@ -10,6 +12,82 @@ namespace ContactBook_V2
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Contacts contacts = new Contacts();
+
+            bool Running = true;
+            bool invalidInput = false;
+            bool firstRun = true;
+
+            while (Running)
+            {
+                Console.Clear();
+                if (invalidInput)
+                {
+                    ColorText.WriteColorLine("Не існуючий варіант, спробуйте ще раз!\n", ConsoleColor.Red);
+                    invalidInput = false;
+                }
+                ColorText.WriteColorLine("-< Книга контактів V2>-\n", ConsoleColor.Yellow);
+                Console.WriteLine("Виберіть дію:");
+
+                Console.WriteLine("1. Додати контакт");
+                Console.WriteLine("2. Вивести всі контакти");
+                Console.WriteLine("3. Видалити контакт");
+                Console.Write("4. Змінити контакт");
+                if (firstRun)
+                {
+                    Console.Write(" <- New");
+                }
+
+                Console.WriteLine("\nesc. Вийти");
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        ColorText.WriteColorLine("Введіть ім'я контакту:", ConsoleColor.Cyan);
+                        string name = Console.ReadLine();
+                        ColorText.WriteColorLine("Введіть номер телефону:", ConsoleColor.Magenta);
+                        string phoneNumber = Console.ReadLine();
+                        contacts.AddContact(name, phoneNumber);
+                        ColorText.WriteColorLine("\nНатисніть будь-яку клавішу для повернення до меню.", ConsoleColor.DarkGray);
+                        Console.ReadKey(true);
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        contacts.ShowContacts();
+                        ColorText.WriteColorLine("\nНатисніть будь-яку клавішу для повернення до меню.", ConsoleColor.DarkGray);
+                        Console.ReadKey(true);
+                        break;
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        ColorText.WriteColorLine("Введіть ім'я контакту для видалення:", ConsoleColor.DarkGray);
+                        string name2 = Console.ReadLine();
+                        contacts.DeleteContact(name2);
+                        ColorText.WriteColorLine("\nНатисніть будь-яку клавішу для повернення до меню.", ConsoleColor.DarkGray);
+                        Console.ReadKey(true);
+                        break;
+                    case ConsoleKey.D4:
+                        Console.Clear();
+                        ColorText.WriteColorLine("Введіть ім'я контакту для зміни:", ConsoleColor.DarkGray);
+                        string oldName = Console.ReadLine();
+                        ColorText.WriteColorLine("Введіть нове ім'я контакту:", ConsoleColor.Cyan);
+                        string newName = Console.ReadLine();
+                        ColorText.WriteColorLine("Введіть новий номер телефону:", ConsoleColor.Magenta);
+                        string newPhoneNumber = Console.ReadLine();
+                        contacts.ChangeContact(oldName, newName, newPhoneNumber);
+                        ColorText.WriteColorLine("\nНатисніть будь-яку клавішу для повернення до меню.", ConsoleColor.DarkGray);
+                        Console.ReadKey(true);
+                        break;
+                    case ConsoleKey.Escape:
+                        Running = false;
+                        break;
+                    default:
+                        invalidInput = true;
+                        break;
+                }
+                firstRun = false;
+            }
         }
     }
 }
